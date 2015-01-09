32d8abfda520ef6e4a684ef388590e60dddf7132
syotaro <yamamoto@i-trenta.net>
2015-01-09 22:59:23 +0900

    fix

---
+++ setup_daily.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+vim +NeoBundleInstall +qall
+brew update
+brew cask update
+
+
+open /Applications/App Store.app
+open /Applications/QuickTime Player.app  # for iphone record
+open ~/Applications/Flux.app
+open /opt/homebrew-cask/Caskroom/google-drive/latest/Google Drive.app
+open /Applications/CopyLess.app
