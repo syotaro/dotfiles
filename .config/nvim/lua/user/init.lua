1144574e32915ef11d2eb12b2b2d6234998c8ec0
syotaro <y@syotaro.biz>
2022-11-12 16:24:45 +0900

    fix

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -324,8 +324,8 @@ local config = {
       { "godlygeek/tabular" },
       { "preservim/vim-markdown" },
       { "rbtnn/vim-ambiwidth" },
-      { 'dsznajder/vscode-es7-javascript-react-snippets', run = 'yarn install --frozen-lockfile && yarn compile' } -- https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
-
+      { 'dsznajder/vscode-es7-javascript-react-snippets', run = 'yarn install --frozen-lockfile && yarn compile' }, -- https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
+      { 'junegunn/vim-easy-align' },
       -- { "andweeb/presence.nvim" },
       -- {
       --   "ray-x/lsp_signature.nvim",
